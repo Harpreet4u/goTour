ad1fdf4d8d1d5fb3a75483e1ca0c7c8b092a649b
harpreet4u <happysinghsingh7@gmail.com>
2015-07-22 06:51:57 +0530

    short var declaration in go

---
+++ short-var-declarations.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var i, j int = 1, 2
+    // Alternative approach
+    k := 3 // Yo ;)
+    c, python, java := true, false, "no!"
+    
+    fmt.Println(i, j, k, c, python, java)
+}
