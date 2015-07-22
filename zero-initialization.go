f55b553c855a62c8152050b05932696972d051d8
harpreet4u <happysinghsingh7@gmail.com>
2015-07-22 06:44:52 +0530

    Zero intialization in go

---
+++ zero-initialization.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// In go int are initialized with 0, float 0.0 bool false others nil
+var c, python, java bool
+
+func main() {
+    var i int
+    fmt.Println(i, c, python, java)
+}
