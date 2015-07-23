6e1d526a1feec2a04c68cc23cfba68ca5f4a2408
harpreet4u <happysinghsingh7@gmail.com>
2015-07-23 06:35:25 +0530

    GoStrings basics

---
+++ GoStrings/goStrings.go
@@ -0,0 +1,36 @@
+/*
+    Strings --> immutable
+    ASCII --> 1 byte (uint8)
+    UTF-8 --> 2-4 bytes (int32) (rune)
+
+    len terminated and variable width unlike c++, java (fixed width)
+    less disk space
+*/
+package main
+
+import (
+    f "fmt" // Package aliasing
+)
+
+func main() {
+    var str string // Default value is ""
+
+    f.Println("Default value: ", str)
+    
+    // Interpreted strings. ""
+    str = "This is \n in newline"
+    f.Println(str)
+    f.Println("")
+    
+    // Raw strings. `` 
+    str = `This is \n in same line`
+    f.Println(str)
+   
+    f.Println("")
+    f.Printf("Integer: %d\n", str[0])
+    f.Printf("Character: %c\n", str[0])
+    f.Printf("UTF-8 bytes(int16 or int -- 2 bytes): %X\n", str[0])
+    f.Printf("UTF-8 code points(int32 -- 4 bytes): %U\n", str[0])
+}
+
+
