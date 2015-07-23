ae45d873bb5672b47db7981c36615bf7adceb2bb
harpreet4u <happysinghsingh7@gmail.com>
2015-07-23 22:48:20 +0530

    struct and struct alias type conversion in GO

---
+++ AdvanceStructsAndMethods/structs_conversion.go
@@ -0,0 +1,22 @@
+/* 
+    Go follows strict rules in convertions
+    struct and struct alias can be converted to each other
+*/
+package main
+import "fmt"
+
+type number struct{
+    f float32
+}
+
+type nr number
+
+func main() {
+    a := number{5.0}
+    b := nr{5.0}
+    // var i float32 = b // compile-error
+    // var i = float32(b) // compile-error cannot be converted 
+    // var c number = b // compile-error
+    var c = number(b)
+    fmt.Println(a, b, c)
+}   
